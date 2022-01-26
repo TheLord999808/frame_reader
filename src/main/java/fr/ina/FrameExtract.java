14f445f9a7f34fc5e8cb5b10860a7fc29560b098
TheLord999808 <87827286+TheLord999808@users.noreply.github.com>
2022-01-26 10:31:26 +0100

    FN and T change

--- src/main/java/fr/ina/FrameExtract.java
+++ src/main/java/fr/ina/FrameExtract.java
@@ -1,7 +1,8 @@
 package fr.ina;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
@@ -9,10 +10,22 @@ import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.Frame;
 import org.bytedeco.javacv.Java2DFrameConverter;
 
-public class FrameExtract {
+	public class FrameExtract {
+	
     public static void main(String []args) throws Exception
     {
-        File myObj = new File("D:\\file_example_MP4_1920_18MG.mp4");
+    	System.out.println("Veuillez entre le nom du fichier et son chemin d'accés");
+    	Scanner filepath = new Scanner(System.in);
+    	String filename = filepath.nextLine();
+    	System.out.println("Veuillez entre le temps de la frame que vous voulez extraire");
+    	Scanner th = new Scanner(System.in);
+    	int h = th.nextInt();
+    	Scanner tmin = new Scanner(System.in);
+    	int min = tmin.nextInt();
+    	Scanner tsec = new Scanner(System.in);
+    	int sec = tsec.nextInt();
+    	String extime = h":"min":"sec;
+    	File myObj = new File(filename);
         FFmpegFrameGrabber frameGrabber = new FFmpegFrameGrabber(myObj.getAbsoluteFile());
         frameGrabber.start();
         Frame f;
